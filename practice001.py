d1dfb0d5e94c8c95bdf8c94d25202b0188b10847
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2022-12-14 04:21:08 +0300

    added task 5

--- practice001.py
+++ practice001.py
@@ -1,9 +1,10 @@
 
 # 1) Write a program that takes a number as an input representing the day of the week 
 # and checks whether that day is week
-print('Task 1\n')
+print('Task 1')
+print('------\n')
 def day_checker():
-    weekday = int(input('Enter weekday number (1-7): '))
+    weekday = int(input('Enter any weekday number (1-7): '))
 
     if weekday in range(1, 6):
         print('Weekday\n')
@@ -14,12 +15,13 @@ def day_checker():
     else:
         print('Please enter any weekday number (1-7)\n')
 
-# day_checker()        
+day_checker()        
 
 
 # 2) Write a program for verification of the truth of the statement 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z for all values of the predicate
-print('Task 2\n')
+print('Task 2')
+print('------\n')
 def input_numbers(x):
     xyz = ['X', 'Y', 'Z']
     arr = []
@@ -37,77 +39,96 @@ def predicate_checker():
     x=int(input('Enter the number (<=3): '))
     
     if x > 3 or x < 0:
-        print('Try again')
+        print('Try again\n')
     else:
         statement = input_numbers(x=x)
     
         if check_predicate(statement) == True:
-            print('Statement is true')
+            print('Statement is true\n')
         else:
-            print('Statement is false')
+            print('Statement is false\n')
 
-# predicate_checker()
+predicate_checker()
 
 
 # 3) Write a program that takes coordinates of a point (X and Y) as an input, 
 # with X ≠ 0 and Y ≠ 0, and outputs the quadrant of the plane 
 # in which this point is located (or on which axis it is located)
-print('Task 3\n')
+print('Task 3')
+print('------\n')
 def quadrant(x, y):
     if (x > 0 and y > 0):
-        print ('lies in First quadrant')
+        print ('lies in First quadrant\n')
  
     elif (x < 0 and y > 0):
-        print ('lies in Second quadrant')
+        print ('lies in Second quadrant\n')
          
     elif (x < 0 and y < 0):
-        print ('lies in Third quadrant')
+        print ('lies in Third quadrant\n')
      
     elif (x > 0 and y < 0):
-        print ('lies in Fourth quadrant')
+        print ('lies in Fourth quadrant\n')
          
     elif (x == 0 and y > 0):
-        print ('lies at positive Y axis')
+        print ('lies at positive Y axis\n')
      
     elif (x == 0 and y < 0):
-        print ('lies at negative Y axis')
+        print ('lies at negative Y axis\n')
      
     elif (y == 0 and x < 0):
-        print ('lies at negative X axis')
+        print ('lies at negative X axis\n')
      
     elif (y == 0 and x > 0):
-        print ('lies at positive X axis')
+        print ('lies at positive X axis\n')
      
     else:
-        print ('lies at origin')
+        print ('lies at origin\n')
  
 def quadrant_checker():
     x=float(input('Enter X-coordinate: '))
     y=float(input('Enter Y-coordinate: '))
     quadrant(x, y)
 
-# quadrant_checker()    
+quadrant_checker()    
 
 
 # 4) Write a program that, given a quadrant, shows the range of 
 # possible coordinates of points in that quadrant (x and y)
-print('Task 4\n')
+print('Task 4')
+print('------\n')
 def range_checker():
     quad=int(input('Enter the quadrant (1, 2, 3, 4): '))
     
     if quad == 1:
-        print ('x > 0 and y > 0')
+        print ('x > 0 and y > 0\n')
  
     elif quad == 2:
-        print ('x < 0 and y > 0')
+        print ('x < 0 and y > 0\n')
          
     elif quad == 3:
-        print ('x < 0 and y < 0')
+        print ('x < 0 and y < 0\n')
      
     elif quad == 4:
-        print ('x > 0 and y < 0')
+        print ('x > 0 and y < 0\n')
 
     else:
-        print ('Try again')    
+        print ('Try again\n')    
  
 range_checker()
+
+
+# 5) Write a program that takes the coordinates of two points as an input
+# and finds the distance between them in 2D space
+print('Task 5')
+print('------\n')
+import math
+def euclidean_distance():
+    x1=float(input('Enter X1-coordinate: '))
+    y1=float(input('Enter Y1-coordinate: '))
+    x2=float(input('Enter X2-coordinate: '))
+    y2=float(input('Enter Y2-coordinate: '))
+
+    dist = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2) * 1.0)
+    print(f'Euclidean_distance: {dist}') 
+
+euclidean_distance()
