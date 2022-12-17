85bd5fb1df782091613828b20ee76b871e83fde8
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2022-12-17 16:57:10 +0300

    added task 2

--- practice002.py
+++ practice002.py
@@ -1,7 +1,8 @@
-# 1) The progtam takes an intager as an input and 
-# displays their sum
+# 1) The program takes an intager as an input and 
+# displays the sum of its digits
 print('\nTask 1')
 print('------\n')
+
 def int_sum():
     n = input('Enter a number: ')
     result = 0
@@ -12,7 +13,26 @@ def int_sum():
 # int_sum()    
 
 
-# 2) The progtam takes an intager as an input and 
-# displays their sum
+# 2) The program generates a list of integers based on the sequence 
+# (1 + 1/n)**n and displays the sum of its elements
 print('\nTask 2')
 print('------\n')
+
+def seq_generator(n):
+    for x in range(1, n+1):
+        yield round((1 + 1/x)**x, 2)
+        
+def sum_calculator():
+    n = int(input('Enter a number: '))
+    
+    arr = []
+    for x in seq_generator(n):
+        arr.append(x)
+    print(arr)
+
+    result = 0
+    for i in arr:
+        result += i
+    print(f'Sum of digits in the number: {result}\n')    
+
+sum_calculator()    
