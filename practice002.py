e9d0ef0711e8b7b45e3401a817bb688d4bcd542e
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2022-12-17 16:09:28 +0300

    added task 1

---
+++ practice002.py
@@ -0,0 +1,18 @@
+# 1) The progtam takes an intager as an input and 
+# displays their sum
+print('\nTask 1')
+print('------\n')
+def int_sum():
+    n = input('Enter a number: ')
+    result = 0
+    for i in range(len(n)):
+        result += int(n[i])
+    print(f'Sum of digits in the number: {result}\n')
+
+# int_sum()    
+
+
+# 2) The progtam takes an intager as an input and 
+# displays their sum
+print('\nTask 2')
+print('------\n')
