ed241f6369c8ca761d0fc90d19037b2aa143c306
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2022-12-22 19:19:53 +0300

    added 2nd task

--- practice003.py
+++ practice003.py
@@ -7,7 +7,7 @@ def even_odd_sum():
     n = int(input('Enter number of elements: '))
  
     for i in range(n):
-        item = int(input(f'Element {i+1}: '))
+        item = int(input(f'Element {i + 1}: '))
         lst.append(item)
 
     even = 0
@@ -18,17 +18,56 @@ def even_odd_sum():
         else:
             odd += lst[j]
      
-    print (f'Even index positions sum: {even}' )
+    print (f'\nEven index positions sum: {even}' )
     print (f'Odd index positions sum: {odd}\n')
  
-even_odd_sum()
+# even_odd_sum()
 
 
-# # 2) The program generates a list of integers based on the sequence 
-# # (1 + 1/n)**n and displays the sum of its elements
-# print('\nTask 2')
-# print('------\n')
+# 2) The program multiplies pairs of first - last, second - anterior to last etc.
+print('\nTask 2')
+print('------\n')
 
+def reverse_list(lst, start, end):
+    while start < end:
+        lst[start], lst[end] = lst[end], lst[start]
+        start += 1
+        end -= 1
+    
+    return lst    
+
+def remove_middle(lst):
+    idx = 0
+    size = len(lst)
+    idx = size // 2
+
+    if (size % 2 == 1):
+        lst.pop(idx)
+
+    return lst
+
+def multiply_pairs():
+    dir_lst = []
+    n = int(input('Enter number of elements: '))
+ 
+    for i in range(n):
+        item = int(input(f'Element {i + 1}: '))
+        dir_lst.append(item)
+
+    dir_lst = remove_middle(dir_lst)
+    rev_lst = reverse_list(dir_lst.copy(), 0, len(dir_lst)-1)
+
+    mul_lst = []
+    for j in range(0, len(dir_lst)):
+        mul_lst.append(dir_lst[j] * rev_lst[j])
+
+    mul_lst_print = mul_lst[: len(mul_lst) // 2]
+
+    print (f'\nOriginal list: {dir_lst}')
+    print (f'Reversed list: {rev_lst}')  
+    print (f'List of multiplied pairs: {mul_lst_print}\n')
+
+multiply_pairs()
 
 
 # # 3) The program shuffles a list of numbers
