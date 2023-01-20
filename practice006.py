fc56f3b3d31236585b4756513d1beb38faff456a
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2023-01-20 05:53:35 +0300

    added 2st task

--- practice006.py
+++ practice006.py
@@ -2,17 +2,21 @@
 print('\nTask 1')
 print('------\n')
 
-def odd_even():
-    lst1 = [int(item) for item in str(input("Enter the list items: "))]
+def even_odd_sum():
+    lst = [int(item) for item in str(input("Enter the list items: "))]
     
     even = 0
     odd = 0
 
-    for j in range(len(lst)):
-        if j % 2 == 0:
-            even += lst[j]
-        else:
-            odd += lst[j]
+    for j, k in zip(lst, lst):
+        
+        even_list = list(filter(lambda j: j % 2 == 0, lst))
+        for e_idx, e_item in enumerate(even_list):
+            even += even_list[e_idx]
+        
+        odd_list = list(filter(lambda k: k % 2 != 0, lst))
+        for o_idx, o_item in enumerate(odd_list):
+            odd += odd_list[o_idx]
     
     print (f'\nEven index positions sum: {even}')
     print (f'Odd index positions sum: {odd}\n')
@@ -20,47 +24,67 @@ def odd_even():
 even_odd_sum()
 
 
-# 2) The program multiplies pairs of array elements:
-# first - last, second - anterior to last etc.
-print('\nTask 2')
-print('------\n')
+# # 2) The program multiplies pairs of array elements:
+# # first - last, second - anterior to last etc.
+# print('\nTask 2')
+# print('------\n')
 
-def reverse_list(lst, start, end):
-    while start < end:
-        lst[start], lst[end] = lst[end], lst[start]
-        start += 1
-        end -= 1
+# def reverse_list(lst, start, end):
+#     while start < end:
+#         lst[start], lst[end] = lst[end], lst[start]
+#         start += 1
+#         end -= 1
     
-    return lst    
+#     return lst    
 
-def remove_middle(lst):
-    idx = 0
-    size = len(lst)
-    idx = size // 2
+# def remove_middle(lst):
+#     idx = 0
+#     idx = len(lst) // 2
 
-    if (size % 2 == 1):
-        lst.pop(idx)
+#     if (len(lst) % 2 == 1):
+#         lst = lambda idx: lst.pop(idx)
 
-    return lst
+#     return lst
 
-def multiply_pairs():
+# def multiply_pairs():
+#     dir_lst = [int(item) for item in str(input("Enter the list items: "))]    
+
+#     dir_lst = remove_middle(dir_lst)
+#     rev_lst = reverse_list(dir_lst.copy(), 0, len(dir_lst) - 1)
+
+#     mul_lst = []
+#     for j,k in enumerate(dir_lst):
+#         mul_lst.append(dir_lst[j] * rev_lst[j])
+
+#     mul_lst_print = mul_lst[: len(mul_lst) // 2]
+
+#     print (f'\nOriginal list having pairs: {dir_lst}')
+#     print (f'List of multiplied pairs: {mul_lst_print}\n')
+
+# # multiply_pairs()
+
+
+# 3) The program extracts the fractional parts of float numbers
+# and finds difference between their max and min (if value is not zero) 
+print('\nTask 3')
+print('------\n')
+def fractional_difference():
     n = int(input('Enter number of elements: '))
     
-    dir_lst = []
+    lst_float = []
     for i in range(n):
-        item = int(input(f'Element {i + 1}: '))
-        dir_lst.append(item)
+        item = float(input(f'Element {i + 1}: '))
+        lst_float.append(item)
+    
+    lst_frac = []
+    for j in range(n):
+        frac = lst_float[j] % 1
+        lst_frac.append(frac)
 
-    dir_lst = remove_middle(dir_lst)
-    rev_lst = reverse_list(dir_lst.copy(), 0, len(dir_lst) - 1)
+    lst_filtered = [x for x in lst_frac if x != 0]
+    lst_sorted = sorted(lst_filtered) 
 
-    mul_lst = []
-    for j in range(0, len(dir_lst)):
-        mul_lst.append(dir_lst[j] * rev_lst[j])
+    diff = round((lst_sorted[-1] - lst_sorted[0]), 2)
+    print (f'\nDifference between max and min of fractional parts: {diff}\n')
 
-    mul_lst_print = mul_lst[: len(mul_lst) // 2]
-
-    print (f'\nOriginal list having pairs: {dir_lst}')
-    print (f'List of multiplied pairs: {mul_lst_print}\n')
-
-multiply_pairs()
+fractional_difference()
